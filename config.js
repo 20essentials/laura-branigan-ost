5740f9627321331a07c9e6ffb11c2f47dab3bf57
DominicDev <s0l0estudi4r@gmail.com>
2025-11-22 16:33:16 +0100

    v0.0.2

---
+++ config.js
@@ -0,0 +1,23 @@
+export const nameBand = 'LAURA BRANIGAN';
+export let nameSongs = [
+	"SELF CONTROL", /*1*/
+	"GLORIA", /*2*/
+	"SELF CONTROL - GTA VICE CITY", /*3*/
+	"TI AMO", /*4*/
+	"POWER OF LOVE", /*5*/
+	"THE LUCKY ONE", /*6*/
+	"SPANISH EDDIE", /*7*/
+	"OVER YOU", /*8*/
+	"DIDN'T WE ALMOST WIN IT ALL", /*9*/
+	"HOW AM I SUPPOSED TO LIVE ", /*10*/
+	"SOLITAIRE", /*11*/
+	"SHATTERED GLASS", /*12*/
+	"FOREVER YOUNG", /*13*/
+	"CRY WOLF", /*14*/
+	"HOLD ME", /*15*/
+	"SHADOW OF LOVE", /*16*/
+	"NO PROMISE-NO GUARANTEE", /*17*/
+	"LET ME IN", /*18*/
+	"MUJER CONTRA MUJER", /*19*/
+	"ONLY TIME WILL TELL", /*20*/
+]
